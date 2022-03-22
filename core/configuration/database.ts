c18e41f803ec3444bbe6d1c420a96828b7d008f9
zdllucky <animereader@yandex.kz>
2022-03-23 03:04:48 +0600

    git secret fix

--- core/configuration/database.ts
+++ core/configuration/database.ts
@@ -6,7 +6,7 @@ const db: DatabaseConfig<BaseKeystoneTypeInfo> = {
   url: `postgres://${e`POSTGRES_USER`}:${e`POSTGRES_PASSWORD`}@${e`POSTGRES_HOST`}:5432/${e`POSTGRES_DB`}`,
   useMigrations: true,
   idField: { kind: "cuid" },
-  async onConnect(ctx) {
+  async onConnect() {
     console.log("Seeding complete!");
   },
 };
