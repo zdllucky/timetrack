4ff9c99f44e8651139e66a1f251f26042f0953f7
zdllucky <animereader@yandex.kz>
2022-09-28 04:20:48 +0600

    Provided department manager & user test cases

--- core/schema/Department/Department_User.api.test.ts
+++ core/schema/Department/Department_User.api.test.ts
@@ -1,21 +1,84 @@
 import { setupTestEnv, TestEnv } from "@keystone-6/core/testing";
 import { KeystoneContext } from "@keystone-6/core/types";
 import config from "../../keystone";
+import { provide } from "../_misc/helpers/testHelpers";
+import {
+  Department,
+  Maybe,
+  MutationCreateDepartmentArgs,
+  User,
+} from "../../schema_types";
+import { gql } from "@keystone-6/core";
 
 describe("User", () => {
   let testEnv: TestEnv;
   let context: KeystoneContext;
+  let user: Maybe<User>;
 
   beforeAll(async () => {
     testEnv = await setupTestEnv({ config });
     context = testEnv.testArgs.context;
 
     await testEnv.connect();
+
+    user = await provide.user(context);
   });
 
   afterAll(async () => {
     await testEnv.disconnect();
   });
 
-  it.todo("can query departments");
+  it("can query departments", async () => {
+    const uc = provide.session(context, user);
+
+    await context.sudo().query.Department.createOne(<
+      MutationCreateDepartmentArgs
+    >{
+      data: {
+        name: "test21",
+        isControlledBy: {
+          create: {
+            name: "test22",
+            isControlledBy: {
+              create: {
+                name: "test23",
+                workers: { connect: [{ login: "testUser" }] },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const queryRes = await uc.graphql.raw({
+      query: gql`
+        query {
+          departments {
+            name
+            heads {
+              login
+            }
+            workers {
+              login
+            }
+            managers {
+              login
+            }
+            isControlledBy {
+              name
+            }
+            controls {
+              name
+            }
+          }
+        }
+      `,
+    });
+
+    console.log(JSON.stringify(queryRes));
+
+    expect(queryRes.errors).toBeUndefined();
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    expect((queryRes.data!.departments as Department[]).length).toBe(3);
+  });
 });
